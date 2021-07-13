4fa9577fe73a5096b09c61eb9b91e125803e7cc1
skEngwEs <SkengJesus@MacBooks-MacBook-Pro.local>
2021-07-13 10:09:41 +0100

    first commit

---
+++ pages/EOM/index.js
@@ -0,0 +1,31 @@
+export const EOM = ({ employee }) => {
+    console.log(employee) 
+    return(
+        <div className='page-container'>
+            <div>
+                <h1>Employee Of The Month</h1>
+            </div>
+            <div>
+                <h3>{employee.name}</h3>
+                <h6>{employee.position}</h6>
+                <img src={employee.image} />
+                <h5>{employee.description   }</h5>
+            </div>
+        </div>
+    )
+}
+
+export const getServerSideProps = async pageContext =>{
+    const res = await fetch(
+        'https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth')
+    const employee = await res.json()
+    
+
+    return {
+        props: {
+            employee: employee
+        }
+    }
+}
+
+export default EOM 
