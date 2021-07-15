2aebe2b9f73388f9d0678528da2daa76b8847ce3
skEngwEs <SkengJesus@MacBooks-MacBook-Pro.local>
2021-07-15 11:53:49 +0100

    navbar to navigate to new pages

--- pages/EOM/index.js
+++ pages/EOM/index.js
@@ -1,11 +1,13 @@
+import styles from '../../styles/EOM.module.css'
+
 export const EOM = ({ employee }) => {
     console.log(employee) 
     return(
         <div className='page-container'>
-            <div>
+            <div className={styles.main}> 
                 <h1>Employee Of The Month</h1>
             </div>
-            <div>
+            <div className={styles.employeeOfTheMonth}>
                 <h3>{employee.name}</h3>
                 <h6>{employee.position}</h6>
                 <img src={employee.image} />
